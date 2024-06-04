2bd3e9fcd6300d6974ea73634515815865191f51
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2024-06-04 13:15:48 +0100

    Database connected successfully

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
 require("dotenv").config();
+
+// DB connection
+connectDB();
 
 app.listen("5000", () => {
   console.log("Backend running...");
