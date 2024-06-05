ca25e44567d4b357f84306c07e63f27b67b3dcda
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2024-06-05 23:47:36 +0100

    password hashed

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,19 +1,20 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 exports.register = async (req, res) => {
-  const { username, email, password, profilePic } = req.body;
-
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedpass = await bcrypt.hash(req.body.password, salt);
+
     const newUser = new User({
-      username,
-      email,
-      password,
-      profilePic,
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedpass,
     });
 
     await newUser.save();
 
-    res.status(201).json({ success: true });
+    res.status(201).json({ success: true, user: newUser });
   } catch (error) {
     res.status(500).json(error);
   }
