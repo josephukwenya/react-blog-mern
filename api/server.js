114af234b0e5006d5eee05b6b405f943402578d5
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2024-06-19 22:13:56 +0100

    added categories route

--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ const connectDB = require("./config/db");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoriesRoute = require("./routes/categories");
 
 dotenv.config();
 app.use(express.json());
@@ -17,6 +18,7 @@ connectDB();
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/posts", postRoute);
+app.use("/api/v1/categories", categoriesRoute);
 
 app.listen("5000", () => {
   console.log("Backend running...");
