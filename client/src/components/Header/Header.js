a43677c095f95e584bfb6b1d6936da90ce402a3e
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2024-06-22 23:59:15 +0100

    building components

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -6,6 +6,11 @@ export default function Header() {
       <div className="headerTitles">
         <span className="headerTitleSm">React & Node</span>
         <span className="headerTitleLg">Blog</span>
+        <img
+          className="headerImg"
+          src="https://images.pexels.com/photos/7773744/pexels-photo-7773744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
       </div>
     </div>
   );
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,11 +1,14 @@
 import Header from "../../components/Header/Header";
+import Posts from "../../components/Posts/Posts";
+import Sidebar from "../../components/Sidebar/Sidebar";
 import "./home.css";
 
 export default function Home() {
   return (
     <div className="home">
       <Header />
-      Homepage
+      <Posts />
+      <Sidebar />
     </div>
   );
 }
