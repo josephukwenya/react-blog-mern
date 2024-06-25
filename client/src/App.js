0d40d9488a6d8f5c7e7705949df38fc315f4394a
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2024-06-25 12:30:58 +0100

    added settings page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import Topbar from "./components/Topbar/Topbar";
+import Settings from "./pages/settings/Settings";
 import Write from "./pages/write/Write";
 
 function App() {
   return (
     <>
       <Topbar />
-      <Write />
+      <Settings />
     </>
   );
 }
